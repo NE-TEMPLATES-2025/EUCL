8576f0ed5976fd68cd5e5952c4cbe640a75a1f08
PaccyC <abayisengaaimepacifique@gmail.com>
2025-05-13 17:59:44 +0200

    displaying of tokens

--- src/main/java/com/paccy/eucl/services/ITokenService.java
+++ src/main/java/com/paccy/eucl/services/ITokenService.java
@@ -2,10 +2,14 @@ package com.paccy.eucl.services;
 
 import com.paccy.eucl.entities.Token;
 import com.paccy.eucl.request.PurchaseElectricityRequest;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
 
 
 public interface ITokenService{
     Token purchaseElectricity(PurchaseElectricityRequest purchaseElectricityRequest);
+    String validateToken(String token);
+    Page<Token> getAllTokensByMeterNumber(UUID meterNumberId, int page, int size);
 }
